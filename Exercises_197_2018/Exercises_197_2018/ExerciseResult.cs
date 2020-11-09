0ce3b190de9a50855f26b21cadbf28642216bd38
JOVANA <197-2018@ftn.edu.rs>
2020-11-09 12:36:46 +0100

    Kreirana klasa ExerciseResult, sa poljima: Id, StudentName, StudentIndex i Points Kreirana metoda toString i konstruktor.

---
+++ Exercises_197_2018/Exercises_197_2018/ExerciseResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_197_2018
+{
+    class ExerciseResult
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Points { get; set; }
+
+        public override string ToString()
+        {
+            return Id + " : " + StudentName + " , " + StudentIndex + " : " + Points;
+        }
+        public ExerciseResult (int Id, string StudentName, string StudentIndex, int Points)
+        {
+            this.Id = Id;
+            this.StudentName = StudentName;
+            this.StudentIndex = StudentIndex;
+            this.Points = Points;
+        }
+    }
+}
